96730e1c091d9de4c9b690a56b406426b467f4a0
Alejandro Urbina <urbinagonzalezja@gmail.com>
2025-01-12 12:59:14 +0100

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/2020-JAUG',
+    site: 'https://github.com/2020-JAUG.io',
     base: '/restaurant/',
     output: 'static',
 });
