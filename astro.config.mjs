77ba4d566d70d8c6d56be379f752802420e69e65
Alejandro Urbina <urbinagonzalezja@gmail.com>
2025-01-12 12:58:42 +0100

    Fix name site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/2020-JAUG/restaurant',
+    site: 'https://github.com/2020-JAUG',
     base: '/restaurant/',
     output: 'static',
 });
